241c9415d2098247dfbb92c5d98a59afda667698
gjbsaiia <gjs64@case.edu>
2019-12-01 17:49:30 -0500

    starting on interface

--- library_search_app/searchsite/urls.py
+++ library_search_app/searchsite/urls.py
@@ -5,10 +5,10 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('post/', views.books, name='books'),
-    path('$/', views.books, name='books'),
-    path('$/', views.libraries, name='libraries'),
-    path('$/', views.authors, name='authors'),
-    path('$/', views.librarians, name='librarians'),
-    path('$/', views.checked_out, name='checked out oooks'),
-    path('$/', views.users, name='users'),
+    # path('$/', views.books, name='books'),
+    # path('$/', views.libraries, name='libraries'),
+    # path('$/', views.authors, name='authors'),
+    # path('$/', views.librarians, name='librarians'),
+    # path('$/', views.checked_out, name='checked out oooks'),
+    # path('$/', views.users, name='users'),
 ]

d232871951f6c964d43b13097a89182a3ddc9d98
gjbsaiia <gjs64@case.edu>
2019-12-01 13:55:34 -0500

    oops

--- unvLibrary/library_search/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class LibrarySearchConfig(AppConfig):
-    name = 'library_search'
--- unvLibrary/unvLibrary/urls.py
+++ unvLibrary/unvLibrary/urls.py
@@ -17,6 +17,5 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('library_search/', include(library_search.urls)),
     path('admin/', admin.site.urls),
 ]

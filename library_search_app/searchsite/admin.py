c05c27a2aa1de2739fcccc2476ec2491c2c54057
gjbsaiia <gjs64@case.edu>
2019-12-01 17:40:20 -0500

    registering models

--- library_search_app/searchsite/admin.py
+++ library_search_app/searchsite/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from searchsite.models import Library
+from searchsite.models import Book
+from searchsite.models import Author
+from searchsite.models import Librarian
+from searchsite.models import Checks_Out
+from searchsite.models import User
+
 
 # Register your models here.
+admin.site.register(Library)
+admin.site.register(Book)
+admin.site.register(Author)
+admin.site.register(Librarian)
+admin.site.register(Checks_Out)
+admin.site.register(User)
---
+++ library_search_app/searchsite/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('$/', views.books, name='books'),
+    path('$/', views.libraries, name='libraries'),
+    path('$/', views.authors, name='authors'),
+    path('$/', views.librarians, name='librarians'),
+    path('$/', views.checked_out, name='checked out oooks'),
+    path('$/', views.users, name='users'),
+]

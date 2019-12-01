9dc2bfe4e3bae3231f90c9ea0ba08c163234db49
gjbsaiia <gjs64@case.edu>
2019-12-01 17:57:56 -0500

    starting on interface

--- library_search_app/searchsite/urls.py
+++ library_search_app/searchsite/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('post/', views.books, name='books'),
+    path('allbooks/', views.books, name='books'),
     # path('$/', views.books, name='books'),
     # path('$/', views.libraries, name='libraries'),
     # path('$/', views.authors, name='authors'),
--- library_search_app/searchsite/views.py
+++ library_search_app/searchsite/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Library, Book, Author, Librarian, Checks_Out
 
 # Create your views here.
 def index(request):
     return HttpResponse('Hello World!')
+
+def libraries(request):
+    all = Library.objects.all()
+    out = ""
+    for each in all:
+        out += str(each)+"\n"
+    return HttpResponse(out)
+
 
 def books(request):
     return HttpResponse("Hello, I'm a book.")

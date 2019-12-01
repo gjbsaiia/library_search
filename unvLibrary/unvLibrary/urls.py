f4194d3146e0b00ddefae3283f55366573503d23
gjbsaiia <gjs64@case.edu>
2019-12-01 13:51:54 -0500

    oops

--- unvLibrary/unvLibrary/urls.py
+++ unvLibrary/unvLibrary/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('library_search/', include(library_search.urls))
-    path('admin/', admin.site.urls),
+    path('admin/', include(admin.site.urls)),
 ]

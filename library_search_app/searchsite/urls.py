14782b70cc5cfe0a0ba92e5b7a99c1fa6aa8dd77
gjbsaiia <gjs64@case.edu>
2019-12-01 19:33:09 -0500

    workin

--- library_search_app/searchsite/urls.py
+++ library_search_app/searchsite/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.login, name='login')
-    path('menu/', views.index, name='index'),
+    path('', views.login, name='login'),
+    path('index/', views.index, name='index'),
     path('allbooks/', views.books, name='books'),
     # path('$/', views.books, name='books'),
     path('libraries/', views.libraries, name='libraries'),

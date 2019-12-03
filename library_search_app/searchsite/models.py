940a411becce1f5c4eaaeaa95b16ed59d8ff7e6d
gjbsaiia <gjs64@case.edu>
2019-12-02 23:01:58 -0500

    admin models registered

--- library_search_app/searchsite/models.py
+++ library_search_app/searchsite/models.py
@@ -134,13 +134,13 @@ class User(models.Model):
 
 class CheckOut_Manager(models.Manager):
     def new_checkout(self, user, book, library):
-        query = Library_Books.objects.filter(library_name=library.name, book_ID=book.id).exclude(count__lt=1)
+        query = Library_Books.objects.filter(library_name=library, book_ID=book).exclude(count__lt=1)
         if(query):
             lb = Library_Books.objects.get(pk = query[0])
             lb -= 1
             lb.save()
             duedate = date.today() + timedelta(weeks=1)
-            checkout = self.create(user_ID=user.id, book_ID=book.id, library_name=library.name, due=duedate)
+            checkout = self.create(user_ID=user, book_ID=book, library_name=library, due=duedate)
             return checkout
 
 

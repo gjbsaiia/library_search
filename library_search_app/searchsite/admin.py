a06cbe2354057710f83b70fde7f40e371def50c3
gjbsaiia <gjs64@case.edu>
2019-12-02 21:03:50 -0500

    admin models registered

--- library_search_app/searchsite/admin.py
+++ library_search_app/searchsite/admin.py
@@ -9,7 +9,6 @@ from searchsite.models import Publisher
 from searchsite.models import Librarian_Genre
 from searchsite.models import Library_Books
 from searchsite.models import Written_By
-from searchsite.models import Checks_Out
 from searchsite.models import User
 
 # Register your models here.

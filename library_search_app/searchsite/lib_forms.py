04bf0f8450ac2137b0ca17aaf1a5f99abcebdf8a
gjbsaiia <gjs64@case.edu>
2019-12-04 12:04:46 -0500

    some fixes

--- library_search_app/searchsite/lib_forms.py
+++ library_search_app/searchsite/lib_forms.py
@@ -12,12 +12,12 @@ class searchForm(forms.Form):
     libraries = Library.objects.all()
     for each in libraries:
         lib_choices.append((each.name, each.name))
-    library_name = forms.CharField(label='Library',max_length=20, blank=True null=True)
-    publisher_name = forms.CharField(label='Publisher', max_length=20, null=True)
-    book_title = forms.CharField(label='Book Title', max_length=30, null=True)
+    library_name = forms.CharField(label='Library',max_length=20, blank=True, null=True)
+    publisher_name = forms.CharField(label='Publisher', max_length=20, blank=True, null=True)
+    book_title = forms.CharField(label='Book Title', max_length=30, blank=True, null=True)
     genre = forms.ChoiceField(label='Genre',choices=GENRE_CHOICES, initial='')
-    author_name = forms.CharField(label='Author', max_length=20, null=True)
-    date_published = forms.DateField(label="Date Published", null=True)
+    author_name = forms.CharField(label='Author', max_length=20, blank=True, null=True)
+    date_published = forms.DateField(label="Date Published", blank=True, null=True)
 
     def getData(self):
         return self.cleaned_data or None
